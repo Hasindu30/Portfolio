da1897e1dc8481dc494416449fa427a69877ea9e
Hasindu30 <wohchandeepa@std.appsc.sab.ac.lk>
2024-10-01 13:23:21 +0530

    update build fution

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google"; 
 import "./globals.css";
-import StarCanvas from "@/components/main/StarBackground";
 import Navbar from "@/components/main/Navbar";
 import Footer from "@/components/main/Footer";
 
@@ -25,7 +24,7 @@ export default function RootLayout({
         className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`} 
       >
         <Navbar />
-        <StarCanvas />
+        
         {children}
         <Footer />
       </body>
